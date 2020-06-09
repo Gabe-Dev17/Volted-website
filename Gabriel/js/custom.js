8b85675f6719dbcd214f23d67d2296579aca7c97
bigthack <62493258+bigthack@users.noreply.github.com>
2020-06-08 22:04:40 -0400

    final version

--- Gabriel/js/custom.js
+++ Gabriel/js/custom.js
@@ -4,3 +4,12 @@ jQuery(document).ready(function() {
 	// Your custom js code goes here.
 
 });
+$(document).ready(function() {
+	$('#play-video').on('click', function(ev) {
+		
+	  $("#video")[0].src += "&autoplay=1";
+	  ev.preventDefault();
+	  window.scrollBy(0, 750);
+	
+	});
+  });
